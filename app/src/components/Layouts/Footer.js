efff4ea189bb38905a396a5d7b3f4d27051c0bce
udhaya_kiruthika <139203927+UdhayaKiruthika@users.noreply.github.com>
2024-07-22 10:56:58 +0530

    Update Footer.js

--- app/src/components/Layouts/Footer.js
+++ app/src/components/Layouts/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer class="py-1">
       <p className="text-center text-dark mt-1">
-        ECommerce-Cart &copy;2023, All Rights Reserved
+        ECommerce-Cart &copy;2024, All Rights Reserved
       </p>
     </footer>
   );
